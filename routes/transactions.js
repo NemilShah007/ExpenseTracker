9c63f3a0be8db8927595b6fc6b134d41b9a88601
NemilShah007 <shahnemil103@gmail.com>
2020-03-03 17:56:26 +0530

    API Created

---
+++ routes/transactions.js
@@ -0,0 +1,16 @@
+//jshint esversion:6
+
+const express = require('express');
+const router = express.Router();
+const {getTransactions, addTransaction, deleteTransaction} = require("../controllers/transactions");
+
+router
+  .route("/")
+  .get(getTransactions)
+  .post(addTransaction);
+
+router
+  .route("/:id")
+  .delete(deleteTransaction);
+
+module.exports = router;
